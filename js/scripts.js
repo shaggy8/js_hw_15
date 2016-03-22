44e6d9be9de785b8f0f5dfbe514408dba6559780
Shaggy8 <rominus@bigmir.net>
2016-03-22 22:47:31 +0200

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

--- js/scripts.js
+++ js/scripts.js
@@ -4,46 +4,40 @@ jQuery(function() {
   var start = 0;
   var lastStart;
 
-  $('form').submit(search);
 
-  $('.pages').click(function(ev) {
-
-    if ( !$(ev.target).is('.pages_links') ) return;
-
-    if ( $(ev.target).is('.forward') ) {
-      start = lastStart + 8;
-    } else if ( $(ev.target).is('.back') ) {
-      start = lastStart - 8;
-    } else {
-      start = $(ev.target).attr('href') * 8;
-    }
-
-    search(ev);
-
-    if (lastStart >= 56) {
-      $('.forward').hide();
-    } else {
-      $('.forward').show();
-    }
-
-    if (lastStart >= 8) {
-      $('.back').show();
-    } else {
-      $('.back').hide();
-    }
+  $('form').submit(function(event) {
+    event.preventDefault();
+    search();
   });
 
 
-  function search(event) {
+  $('.pages').click(function(event) {
     event.preventDefault();
 
+    var $target = $(event.target);
+
+    if ( !$target.is('.pages_links') || $target.data('thisPage') ) return;
+
+    if ( $target.is('.forward') ) {
+      start = lastStart + 8;
+    } else if ( $target.is('.back') ) {
+      start = lastStart - 8;
+    } else {
+      start = $target.attr('href') * 8;
+    }
+
+    search();
+  });
+
+
+  function search() {
     var request = $('input[type="search"]').val();
     if (request== '') return;
 
     $.getJSON('http://ajax.googleapis.com/ajax/services/search/web?q=' + request + '&v=1.0&rsz=8&hl=ua&start=' + start + '&callback=?',
       function(data){
-
         console.log(data);
+
         var ul = document.createElement("ul");
 
         $.each(data.responseData.results, function(i, val){
@@ -59,10 +53,22 @@ jQuery(function() {
       $('.pages').show();
     });
 
-    $('.pages_links').css({'color': ''});
-    $('.pages_links').eq(start / 8 + 1).css({'color': 'red'});
+    $('.pages_links').css('color', '').data('thisPage', false);
+    $('.pages_links').eq(start / 8 + 1).css('color', 'red').data('thisPage', true);
 
     lastStart = start;
     start = 0;
+
+    if (lastStart >= 56) {
+      $('.forward').hide();
+    } else {
+      $('.forward').show();
+    }
+
+    if (lastStart >= 8) {
+      $('.back').show();
+    } else {
+      $('.back').hide();
+    }
   }
 });
